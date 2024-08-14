d43249d22ac55e53fbf588d0098c7cada80369f9
Kavitha S <54953521+Kavitha162@users.noreply.github.com>
2024-08-14 11:11:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+//selection popupoverlay, popupbox, popupbutton
+var popupoverlay=document.querySelector(".popup-overlay");
+var popupbox=document.querySelector(".popup-box");
+var popbut=document.getElementById("add-popupbutton");
+
+popbut.addEventListener("click",function(){
+    popupoverlay.style.display="block";
+    popupbox.style.display="block";
+
+})
+//selecting cancel botton
+var cancelbook=document.getElementById("cancel-book");
+
+cancelbook.addEventListener("click",function(event){
+    event.preventDefault()
+    popupoverlay.style.display="none";
+    popupbox.style.display="none";
+})
+
+//selecting container, add-book, book-title, book-author, book-description
+
+var container=document.querySelector(".container");
+var addbook=document.getElementById("add-book");
+var booktitle=document.getElementById("book-title");
+var bookauthor=document.getElementById("book-author");
+var bookdescription=document.getElementById("book-description");
+
+addbook.addEventListener("click",function(event){
+    event.preventDefault()
+    var div=document.createElement("div");
+    div.setAttribute("class","book-container");
+    div.innerHTML=`<h2>${booktitle.value}</h2>
+    <h5>${bookauthor.value}</h5>
+    <p>${bookdescription.value}</p>
+    <button onclick="deletebook(event)">Delete</button>`
+    container.append(div)
+    popupoverlay.style.display="none";
+    popupbox.style.display="none";
+
+
+})
+
+function deletebook(event)
+{
+    event.target.parentElement.remove()
+}
